8f44b1cbf5701b855010fddc245674200a2a88aa
AbimaelP <a.bimael2000@hotmail.com>
2022-01-12 13:21:25 -0300

    Update v0.0.5

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -3,18 +3,19 @@ function msgclass1(){
     var ho = data.getHours()
     var mi = data.getMinutes()
     var prox_aula = 60 - mi
+    var botao = document.getElementById('botao_prog')
+    botao.style.opacity = '0'
     var msg = document.getElementById('aula')
-    if (ho == 7 && mi >= 50 && mi <= 59) {
+    if (ho == 7 && mi >= 50 && mi < 60) {
         msg.innerText = `Ja são ${ho}:${mi} Sua aula de inglês esta prestes a começar`
-    }   else if (ho > 7 && ho < 12) {
+    }   else if (ho > 7 && ho < 10) {
             msg.innerHTML = `Agora são ${ho}:${mi}<p>Você esta na aula de inglês agora</p><p> Faltam ${prox_aula} minutos para sua aula de Programação web começar</p>`
             document.getElementById('img_aul').src = "_images/" + "english_cap.png";
-            if (mi >= 50 && mi < 59) {
+            if (ho >= 7 && ho <= 8 && mi >= 50 && mi < 60) {
                 document.getElementById('img_aul').src = "_images/english_cap.png";
-                form = document.getElementById('img_proxaul');
-                form.src = '_images/progrmn_cap.png'
+                document.getElementById('img_proxaul').src = '_images/progrmn_cap.png'
             }
-        }   else if (ho < 13) {
+        }   else if (ho > 9) {
             msg.innerHTML = `Agora são ${ho}:${mi}<p>Você esta na aula de Desenvolvimento Web<p>Faltam ${prox_aula} minutos para sua aula de banco de dados MySQL começar</p>`
             document.getElementById('img_fundo').src = "_images/" + "progrmn_cap.png";
             }
