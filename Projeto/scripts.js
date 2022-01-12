a17372f336fc196123ebc3b48b2b66d3a1e9fa57
AbimaelP <a.bimael2000@hotmail.com>
2022-01-12 09:24:59 -0300

    Update v0.0.4

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -1,7 +1,22 @@
-function msgtrue(){
+function msgclass1(){
     var data = new Date()
-    var hora = data.getHours()
-    var minute = data.getMinutes()
+    var ho = data.getHours()
+    var mi = data.getMinutes()
+    var prox_aula = 60 - mi
     var msg = document.getElementById('aula')
-    msg.innerText = `Let's learn English`
+    if (ho == 7 && mi >= 50 && mi <= 59) {
+        msg.innerText = `Ja são ${ho}:${mi} Sua aula de inglês esta prestes a começar`
+    }   else if (ho > 7 && ho < 12) {
+            msg.innerHTML = `Agora são ${ho}:${mi}<p>Você esta na aula de inglês agora</p><p> Faltam ${prox_aula} minutos para sua aula de Programação web começar</p>`
+            document.getElementById('img_aul').src = "_images/" + "english_cap.png";
+            if (mi >= 50 && mi < 59) {
+                document.getElementById('img_aul').src = "_images/english_cap.png";
+                form = document.getElementById('img_proxaul');
+                form.src = '_images/progrmn_cap.png'
+            }
+        }   else if (ho < 13) {
+            msg.innerHTML = `Agora são ${ho}:${mi}<p>Você esta na aula de Desenvolvimento Web<p>Faltam ${prox_aula} minutos para sua aula de banco de dados MySQL começar</p>`
+            document.getElementById('img_fundo').src = "_images/" + "progrmn_cap.png";
+            }
+    
 }
